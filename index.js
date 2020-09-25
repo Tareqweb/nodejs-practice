bfe883a33fa4c512704c20f61ce98c26cfa4fd1a
Tareqweb <18103045tareq@gmail.com>
2020-09-25 23:14:33 +0600

    practice done

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express=require('express');
-
+const cors=require('cors');
+const bodyParser = require('body-parser');
 
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-// const rootCall=(req,res)=>res.send('Thank you very much');
+const users=["Asad","Tareq","Bareq","Faruq","Foysal","Iqram","Dadu"];
+
 
 
 app.get('/',(req,res)=>{
-    res.send('Thank you for show the result');
+    const fruit={
+        products:'banana',
+        price:120
+    }
+    res.send(fruit);
+});
+
+
+app.get('/users/:id',(req,res)=>{
+    const id=req.params.id;
+    const name=users[id];
+    res.send({id,name});
+})
+
+//post
+app.post('/addUser',(req,res)=>{
+    //save to database
+    const user=req.body;
+    user.id=55;
+    res.send(user);
 });
 
 app.listen(3000,()=>console.log('listening on port 3000'));
